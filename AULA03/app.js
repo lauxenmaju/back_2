56732e304548255344f46bca9d4a8828323e035e
lauxenmaju <161649077+lauxenmaju@users.noreply.github.com>
2024-04-08 08:57:36 -0300

    Add files via upload

---
+++ AULA03/app.js
@@ -0,0 +1,22 @@
+//criar pastas
+
+const fs = require("fs");
+
+//com metodo sincrono
+
+//fs.mkdirSync("logs");
+
+// com metodo assincrono
+
+fs.mkdir("./logs2",(erro)=>{
+    if(erro){
+        console.log("Erro!!");
+    }
+})
+
+//para deletar pastas
+
+if(fs.existsSync("./logs2")){
+    fs.rmdirSync("./logs2");
+}
+console.log("Fim da execução")
